92b57d4944a09859b1dccf8c7794323b03d74ad4
WX-T <154199219@qq.com>
2016-01-27 13:13:06 +0800

    Update notes.php

--- notes.php
+++ notes.php
@@ -1,1 +1,50 @@
+<?php
+ /*
+  *
+  * YII 2.0 student notes
+  *  @auther WX-T
+  *
+  *
+ */
+  
+  //生成Url
+  // /index.php?r=site/index
+  echo Url::toRoute('site/index');
 
+  // /index.php?r=site/index&src=ref1#name
+  echo Url::toRoute(['site/index', 'src' => 'ref1', '#' => 'name']);
+
+  // /index.php?r=post/edit&id=100     assume the alias "@postEdit" is defined as "post/edit"
+  echo Url::toRoute(['@postEdit', 'id' => 100]);
+
+  // http://www.example.com/index.php?r=site/index
+  echo Url::toRoute('site/index', true);
+
+  // https://www.example.com/index.php?r=site/index
+  echo Url::toRoute('site/index', 'https');
+  
+  //Url::to()
+  // /index.php?r=site/index
+  echo Url::to(['site/index']);
+  
+  // /index.php?r=site/index&src=ref1#name
+  echo Url::to(['site/index', 'src' => 'ref1', '#' => 'name']);
+  
+  // /index.php?r=post/edit&id=100     assume the alias "@postEdit" is defined as "post/edit"
+  echo Url::to(['@postEdit', 'id' => 100]);
+  
+  // the currently requested URL
+  echo Url::to();
+  
+  // /images/logo.gif
+  echo Url::to('@web/images/logo.gif');
+  
+  // images/logo.gif
+  echo Url::to('images/logo.gif');
+  
+  // http://www.example.com/images/logo.gif
+  echo Url::to('@web/images/logo.gif', true);
+  
+  // https://www.example.com/images/logo.gif
+  echo Url::to('@web/images/logo.gif', 'https');
+  
