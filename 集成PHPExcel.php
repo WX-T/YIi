ed668bc02eb25845175ea0b486bea51c58ec24d1
WX-T <154199219@qq.com>
2016-02-29 17:16:21 +0800

    Update 集成PHPExcel.php

--- 集成PHPExcel.php
+++ 集成PHPExcel.php
@@ -1,5 +1,5 @@
 <?php
   //基础模板集成
   //1.将phpExcel放到vendor目录
-  //2.在入口脚本处添加，引入phpExcel
-  require(__DIR__ . '/../vendor/PHPExcel/PHPExcel.php');
+  //2.在入口脚本(index.php)处添加，引入phpExcel
+  require(__DIR__ . '/../vendor/PHPExcel/PHPExcel.php'); //示例
